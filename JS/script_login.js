ffe5cf5ffe0caf5d76cb5be34eb42dbed5ec0e34
Gabriel Ferreira <gabriel.severinofe@gmail.com>
2024-05-25 14:57:17 -0300

    upd 1 tcc website

--- JS/script_login.js
+++ JS/script_login.js
@@ -1,34 +1,12 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    var userType = document.getElementById("userType").value;
-    var username = document.getElementById("username").value;
+    var username = document.getElementById("user").value;
     var password = document.getElementById("password").value;
     
-    // Verifica o tipo de usuário e redireciona para a página apropriada
-    switch(userType) {
-        case "aluno":
-            if (username === "aluno" && password === "123") {
-                window.location.href = "aluno.html";
-            } else {
-                alert("Usuário ou senha incorretos para aluno.");
-            }
-            break;
-        case "professor":
-            if (username === "professor" && password === "123") {
-                window.location.href = "professor.html";
-            } else {
-                alert("Usuário ou senha incorretos para professor.");
-            }
-            break;
-        case "coordenacao":
-            if (username === "coordenacao" && password === "123") {
-                window.location.href = "coordenacao.html";
-            } else {
-                alert("Usuário ou senha incorretos para coordenação.");
-            }
-            break;
-        default:
-            alert("Tipo de usuário inválido.");
-    }
+    if (username === "aluno" && password === "123") {
+        window.location.href = "aluno.html";
+        } else {
+            alert("Usuário ou senha incorretos para aluno.");
+        }
 });
